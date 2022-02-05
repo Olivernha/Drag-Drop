2f22feaba1b2ec047a1c594a811d682f2cda5104
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-02-05 21:59:33 +0630

    added a droppable area

--- src/app.ts
+++ src/app.ts
@@ -190,7 +190,8 @@ class ProjectItem
   }
   @autobind
   dragStartHandler(event: DragEvent): void {
-    console.log(event);
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
   }
   dragEndHandler(_: DragEvent): void {
     console.log("DragEnd");
@@ -222,13 +223,17 @@ class ProjectList
     this.renderContent();
   }
   @autobind
-  dragOverHandler(_: DragEvent): void {
-   
-    const listEl = this.element.querySelector("ul")!;
-    listEl.classList.add("droppable");
+  dragOverHandler(event: DragEvent): void {
+    if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+      event.preventDefault();
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
   }
   @autobind
-  dropHandler(_: DragEvent): void {}
+  dropHandler(event: DragEvent): void {
+    event.dataTransfer!.getData("text/plain");
+  }
   @autobind
   dragLeaveHandler(_: DragEvent): void {
     const listEl = this.element.querySelector("ul")!;
